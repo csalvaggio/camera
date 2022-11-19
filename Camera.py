e033116d53622000f7ab7b1cca3cfcb3ab0296bf
Carl Salvaggio <salvaggio@cis.rit.edu>
2022-11-18 21:55:58 -0500

    Initial commit

--- Camera.py
+++ Camera.py
@@ -148,18 +148,118 @@ class Camera(object):
          sys.stdout.flush()
          sys.exit()
 
-   def capture(self):
-      iso8601_time_string = \
-         datetime.datetime.utcnow().isoformat(timespec='milliseconds') + 'Z'
-      iso8601_time_string = iso8601_time_string.replace(":", "-" )
-      iso8601_time_string = iso8601_time_string.replace(".", "-" )
+   def capture(self, basename=None, verbose=False):
+      """
+      IMPORTANT NOTE:
+      When using this capture method, the camera must be set to capture RAW or
+      JPEG only, NOT BOTH
+      """
+      if basename:
+         pass
+      else:
+         # Default basename will be the current ISO8601 time string
+         basename = \
+            datetime.datetime.utcnow().isoformat(timespec='milliseconds') + 'Z'
+         basename = basename.replace(":", "-" )
+         basename = basename.replace(".", "-" )
 
+      # Capture image from gphoto2 camera
+      if verbose:
+         msg = 'capture: '
+         msg += 'Triggering camera ...'
+         msg +=  '\n'
+         sys.stdout.write(msg)
+         sys.stdout.flush()
+      try:
+         camera_filepath = \
+            self._connection.capture(gp.GP_CAPTURE_IMAGE)
+      except:
+         msg = 'capture: '
+         msg += 'gphoto2 capture unsuccessful, try power cycling the camera'
+         msg += '\n'
+         sys.stdout.write(msg)
+         sys.stdout.flush()
+         return None
 
+      # Add extension to the basename to match the image file on the
+      # camera's SD card
+      extension = os.path.splitext(camera_filepath.name)[1].lower()
+      filepath = basename + extension
 
+      # Extract image from the camera's SD card and save to local disk
+      if verbose:
+         msg = 'capture: '
+         msg += 'Extracting image from camera\'s SD card to ...'
+         msg += '\n'
+         msg += '       {0}'.format(filepath)
+         msg += '\n'
+         sys.stdout.write(msg)
+         sys.stdout.flush()
+      try:
+         camera_file = self._connection.file_get(camera_filepath.folder,
+                                                 camera_filepath.name,
+                                                 gp.GP_FILE_TYPE_NORMAL)
+      except:
+         msg = 'capture: '
+         msg += 'Image extraction from camera\'s SD card was unsuccessful'
+         msg += '\n'
+         sys.stdout.write(msg)
+         sys.stdout.flush()
+         return None
 
+      if verbose:
+         msg = 'capture: '
+         msg += 'Saving extracted image to local disk ...'
+         msg += '\n'
+         sys.stdout.write(msg)
+         sys.stdout.flush()
+      try:
+         camera_file.save(filepath)
+      except:
+         msg = 'capture: '
+         msg += 'The save operation to local disk was unsuccessful'
+         msg += '\n'
+         sys.stdout.write(msg)
+         sys.stdout.flush()
+         return None
+
+      # Delete image from the camera's SD card
+      if verbose:
+         msg = 'capture: '
+         msg += 'Deleting image from camera\'s SD card ...'
+         msg += '\n'
+         sys.stdout.write(msg)
+         sys.stdout.flush()
+      try:
+         self._connection.file_delete(camera_filepath.folder,
+                                      camera_filepath.name)
+      except:
+         msg = 'capture: '
+         msg += 'Image deletion operation from camera\'s SD card was '
+         msg += 'unsuccessful'
+         msg += '\n'
+         sys.stdout.write(msg)
+         sys.stdout.flush()
+         return None
+
+      if verbose:
+         msg = 'capture: '
+         msg += 'Complete'
+         msg += '\n'
+         sys.stdout.write(msg)
+         sys.stdout.flush()
+
+      # Clean up memory
+      del camera_file
+      del camera_filepath
+
+      return filepath
 
 
 
 if __name__ == '__main__':
-   filename = 'camera_parameter_files/canon_eos_rebel_xsi.json'
-   c = Camera(filename, verbose=True)
+   camera_parameter_filename = \
+      'camera_parameter_files/canon_eos_rebel_xsi.json'
+   verbose = True
+   c = Camera(camera_parameter_filename, verbose=verbose)
+   filepath = c.capture(verbose=verbose)
